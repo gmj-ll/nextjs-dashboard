a409ede46a15f4900e349f3943c5f802458ebf6a
gmj <mengji2333@gmail.com>
2024-08-06 17:57:20 +0800

    chapter 11

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,18 @@
 /** @type {import('next').NextConfig} */
 
-const nextConfig = {};
+
+const nextConfig = {
+  experimental: {
+    ppr: 'incremental',
+  },
+  async rewrites() {
+    return [
+      {
+        source: "/:path*",
+        destination: "http://127.0.0.1:8080/:path*",
+      },
+    ]
+  },
+};
 
 export default nextConfig;
